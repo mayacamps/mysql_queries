b9f331bfb4399944dd5c582531b94f7dd7ee1e7e
Maya Camps <127402122+mayacamps@users.noreply.github.com>
2023-11-13 10:07:45 +0100

    cross-corrections

--- queries/universidad_mySQL.sql
+++ queries/universidad_mySQL.sql
@@ -14,7 +14,7 @@ SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'profesor' AND tel
 SELECT nombre AS asignatura FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
 
 /*6*/-- Returns a list of professors along with the name of the department to which they are linked. The listing should return four columns, first last name, second last name, first name and department name. The result will be sorted alphabetically from lowest to highest by last name and first name
-SELECT apellido1, apellido2, p.nombre, d.nombre AS departamento FROM persona AS p INNER JOIN profesor AS prof ON p.id = prof.id_profesor INNER JOIN departamento AS d ON prof.id_departamento = d.id WHERE p.tipo = 'profesor' ORDER BY apellido1, apellido2, p.nombre;
+SELECT apellido1, apellido2, p.nombre, d.nombre AS departamento FROM persona AS p INNER JOIN profesor AS prof ON p.id = prof.id_profesor INNER JOIN departamento AS d ON prof.id_departamento = d.id ORDER BY apellido1, apellido2, p.nombre;
 
 /*7*/-- Returns a list with the name of the subjects, start year and end year of the student's school year with NIF 26902806M
 SELECT asig.nombre AS asignatura, c.anyo_inicio, c.anyo_fin FROM asignatura AS asig INNER JOIN alumno_se_matricula_asignatura AS asma ON asig.id = asma.id_asignatura INNER JOIN curso_escolar AS c ON asma.id_curso_escolar = c.id INNER JOIN persona as p ON p.id = asma.id_alumno WHERE p.nif = '26902806M';
@@ -23,7 +23,7 @@ SELECT asig.nombre AS asignatura, c.anyo_inicio, c.anyo_fin FROM asignatura AS a
 SELECT DISTINCT d.nombre AS departamento FROM departamento AS d INNER JOIN profesor AS prof ON d.id = prof.id_departamento INNER JOIN asignatura AS asig ON prof.id_profesor = asig.id_profesor INNER JOIN grado ON asig.id_grado = grado.id WHERE grado.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
 
 /*9*/-- Returns a list of all students who have enrolled in a subject during the 2018/2019 school year
-SELECT DISTINCT apellido1, apellido2, nombre FROM persona INNER JOIN alumno_se_matricula_asignatura AS asma ON persona.id = asma.id_alumno INNER JOIN curso_escolar AS c ON asma.id_curso_escolar = c.id WHERE tipo = 'alumno' AND anyo_inicio = 2018;
+SELECT DISTINCT apellido1, apellido2, nombre FROM persona INNER JOIN alumno_se_matricula_asignatura AS asma ON persona.id = asma.id_alumno INNER JOIN curso_escolar AS c ON asma.id_curso_escolar = c.id AND anyo_inicio = 2018;
 
 
 -- LEFT JOIN AND RIGHT JOIN
@@ -32,17 +32,16 @@ SELECT DISTINCT apellido1, apellido2, nombre FROM persona INNER JOIN alumno_se_m
 SELECT d.nombre AS departamento, apellido1, apellido2, p.nombre FROM persona AS p INNER JOIN profesor AS prof ON p.id = prof.id_profesor LEFT JOIN departamento AS d ON prof.id_departamento = d.id ORDER BY d.nombre, apellido1, apellido2, p.nombre;
 
 /*2*/-- Returns a list of professors who are not associated with a department
-SELECT apellido1, apellido2, p.nombre FROM persona AS p INNER JOIN profesor AS prof ON p.id = prof.id_profesor LEFT JOIN departamento AS d ON prof.id_departamento = d.id WHERE d.id IS NULL ORDER BY d.nombre;
+SELECT apellido1, apellido2, p.nombre FROM persona AS p INNER JOIN profesor AS prof ON p.id = prof.id_profesor LEFT JOIN departamento AS d ON prof.id_departamento = d.id WHERE d.id IS NULL;
 
 /*3*/-- Returns a list of departments that do not have associate professors
-SELECT d.nombre AS departamento FROM departamento AS d LEFT JOIN profesor AS prof ON d.id = prof.id_departamento WHERE prof.id_departamento IS NULL ORDER BY d.nombre;
+SELECT d.nombre AS departamento FROM departamento AS d LEFT JOIN profesor AS prof ON d.id = prof.id_departamento WHERE prof.id_departamento IS NULL;
 
 /*4*/-- Returns a list of teachers who do not teach any subjects
 SELECT apellido1, apellido2, p.nombre FROM persona AS p INNER JOIN profesor AS prof ON p.id = prof.id_profesor LEFT JOIN asignatura AS asig ON prof.id_profesor = asig.id_profesor WHERE asig.id_profesor IS NULL ORDER BY prof.id_profesor;
 
 /*5*/-- Returns a list of subjects that do not have an assigned teacher
-SELECT asig.nombre FROM persona AS p INNER JOIN profesor AS prof ON p.id = prof.id_profesor RIGHT JOIN asignatura AS asig ON prof.id_profesor = asig.id_profesor WHERE prof.id_profesor IS NULL;
-
+SELECT asig.nombre FROM profesor AS prof RIGHT JOIN asignatura AS asig ON prof.id_profesor = asig.id_profesor WHERE prof.id_profesor IS NULL;
 
 -- SUMMARY
 /*1*/-- Returns the total number of students there
